0ffb915003a5cfb8b0bfa21a1a376b128f1a8b71
Pedro Cuasqui <Pedro Cuasqui>
2016-05-27 18:23:38 -0500

    solo agrado algunos comentarios

--- app/src/main/java/adaptadores/ArrayAdapterPersonal.java
+++ app/src/main/java/adaptadores/ArrayAdapterPersonal.java
@@ -27,16 +27,17 @@ public class ArrayAdapterPersonal extends ArrayAdapter {
     public View getView(int posisicion, View view, ViewGroup padre) {
         if (view == null) {
             LayoutInflater inflador = LayoutInflater.from(getContext());
+//            aqui busca al objeto tipo item y le añade al inflador.
             view = inflador.inflate(R.layout.item_alarma, null);
         }
 //        ya esta inflado el layout y ahora estamos agregando dos cajas de texto
-        TextView titulo = (TextView) view.findViewById(R.id.lblTituloAlarma);
+//        busca dentro del inflador a los objetos de la interfaz
+        TextView titulo = (TextView) view.findViewById(R.id.lblTituloAlarma);//esto nos retorna un objeto tipo View
         TextView hora = (TextView) view.findViewById(R.id.lblHora);
 //        ya sabemos que nos envian un objeto tipo AlarmasVo
         AlarmasVo alarma = listadoAlarmas.get(posisicion);
         titulo.setText(alarma.getTitulo());
-        String horaConcat = alarma.getHora() + alarma.getMinuto() + " " + alarma.getTipoHorario();
-        hora.setText(horaConcat);
+        hora.setText(alarma.getHora() + alarma.getMinuto() + " " + alarma.getTipoHorario());
         return (view);
     }
 }
