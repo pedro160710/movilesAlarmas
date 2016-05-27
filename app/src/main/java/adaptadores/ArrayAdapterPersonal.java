dd8bac8e215167a99cb7f5019ac3d163716a52d4
Pedro Cuasqui <Pedro Cuasqui>
2016-05-27 17:34:34 -0500

    aun funcional solo cambio en el formato

--- app/src/main/java/adaptadores/ArrayAdapterPersonal.java
+++ app/src/main/java/adaptadores/ArrayAdapterPersonal.java
@@ -15,25 +15,27 @@ import vo.AlarmasVo;
 /**
  * Created by Pedro on 24/05/2016.
  */
-public class ArrayAdapterPersonal extends ArrayAdapter{
-  private  List<AlarmasVo> listadoAlarmas;
+public class ArrayAdapterPersonal extends ArrayAdapter {
+    private List<AlarmasVo> listadoAlarmas;
+
     public ArrayAdapterPersonal(Context context, List<AlarmasVo> objects) {
-        super(context,0, objects);
-        listadoAlarmas= objects;
+        super(context, 0, objects);
+        listadoAlarmas = objects;
     }
+
     @Override
-    public View getView(int posisicion, View view, ViewGroup padre){
-        if(view== null){
+    public View getView(int posisicion, View view, ViewGroup padre) {
+        if (view == null) {
             LayoutInflater inflador = LayoutInflater.from(getContext());
-            view =inflador.inflate(R.layout.item_alarma,null);
+            view = inflador.inflate(R.layout.item_alarma, null);
         }
 //        ya esta inflado el layout y ahora estamos agregando dos cajas de texto
         TextView titulo = (TextView) view.findViewById(R.id.lblTituloAlarma);
         TextView hora = (TextView) view.findViewById(R.id.lblHora);
 //        ya sabemos que nos envian un objeto tipo AlarmasVo
-        AlarmasVo alarma= listadoAlarmas.get(posisicion);
+        AlarmasVo alarma = listadoAlarmas.get(posisicion);
         titulo.setText(alarma.getTitulo());
-        String horaConcat= alarma.getHora()+alarma.getMinuto()+" "+ alarma.getTipoHorario();
+        String horaConcat = alarma.getHora() + alarma.getMinuto() + " " + alarma.getTipoHorario();
         hora.setText(horaConcat);
         return (view);
     }
